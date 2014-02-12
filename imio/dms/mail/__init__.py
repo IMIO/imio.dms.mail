e7641d09c7f109a48279c2c40cb57664a68edfb8
Stephan Geulette <s.geulette@imio.be>
2014-02-12 12:16:28 +0000

    Put back acquisition on dms file

--- imio/dms/mail/__init__.py
+++ imio/dms/mail/__init__.py
@@ -4,3 +4,6 @@ _ = MessageFactory("imio.dms.mail")
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+
+from collective.dms.basecontent import dmsfile
+dmsfile.DmsFile.__ac_local_roles_block__ = False
