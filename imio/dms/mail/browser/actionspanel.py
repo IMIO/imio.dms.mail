cbcaf7cb60a2dd1382cfd8f8b753c96f17cf8b43
Stephan Geulette <s.geulette@imio.be>
2015-03-14 21:37:11 +0000

    Rename actionspanel view. Don't activate now

--- imio/dms/mail/browser/actionspanel.py
+++ imio/dms/mail/browser/actionspanel.py
@@ -2,17 +2,14 @@ from imio.actionspanel.browser.views import ActionsPanelView
 from imio.actionspanel.browser.viewlets import ActionsPanelViewlet
 
 
-class ActionsPanelView(ActionsPanelView):
+class DmsIMActionsPanelView(ActionsPanelView):
     """
       This manage the view displaying actions on context
     """
     def __init__(self, context, request):
         super(ActionsPanelView, self).__init__(context, request)
         # portal_actions.object_buttons action ids to keep
-        # if you define some here, only these actions will be kept
-        self.ACCEPTABLE_ACTIONS = (
-            'test',
-        )
+        self.ACCEPTABLE_ACTIONS = []
 
 
 class ActionsPanelViewlet(ActionsPanelViewlet):
@@ -22,4 +19,4 @@ class ActionsPanelViewlet(ActionsPanelViewlet):
 
     def renderViewlet(self):
         return self.context.restrictedTraverse("@@actions_panel")(useIcons=False, showOwnDelete=False,
-                                                                  showActions=False, showHistory=True)
+                                                                  showActions=False)
