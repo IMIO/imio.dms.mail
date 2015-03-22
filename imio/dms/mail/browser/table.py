415c8c5209e6a8e4ea8775d9e966e594fe4812da
Stephan Geulette <s.geulette@imio.be>
2015-03-22 15:13:31 +0000

    Overrided assigned_group column in listing #10661

--- imio/dms/mail/browser/table.py
+++ imio/dms/mail/browser/table.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 from zope.i18n import translate
+from z3c.table.column import Column
 from collective.dms.basecontent.browser.listing import VersionsTitleColumn
 from collective.dms.scanbehavior.behaviors.behaviors import IScanFields
+from imio.dms.mail import _
 
 
 class VersionsTitleColumn(VersionsTitleColumn):
@@ -20,3 +24,18 @@ class VersionsTitleColumn(VersionsTitleColumn):
             for (name, value) in scan_infos]
 
         return 'title="%s"' % '\n'.join(scan_infos)
+
+
+class AssignedGroupColumn(Column):
+
+    """Column that displays assigned group."""
+
+    header = _("Treating groups")
+    weight = 30
+
+    def renderCell(self, item):
+        if not item.assigned_group:
+            return ''
+        factory = getUtility(IVocabularyFactory, 'collective.task.AssignedGroups')
+        voc = factory(item)
+        return voc.getTerm(item.assigned_group).title
