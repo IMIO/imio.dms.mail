643c3f204cc099d8c97c64b36dd60eb613e716c9
Stephan Geulette <s.geulette@imio.be>
2015-04-23 13:04:28 +0000

    Added actionspanel on contacts

--- imio/dms/mail/browser/actionspanel.py
+++ imio/dms/mail/browser/actionspanel.py
@@ -12,11 +12,31 @@ class DmsIMActionsPanelView(ActionsPanelView):
         self.ACCEPTABLE_ACTIONS = []
 
 
-class ActionsPanelViewlet(ActionsPanelViewlet):
+class DmsIMActionsPanelViewlet(ActionsPanelViewlet):
     """
-        Override render method
+        Override render method for dms incoming mail
     """
 
     def renderViewlet(self):
         return self.context.restrictedTraverse("@@actions_panel")(useIcons=False, showOwnDelete=True,
                                                                   showAddContent=True, showActions=False)
+
+
+class ContactActionsPanelView(ActionsPanelView):
+    """
+      This manage the view displaying actions on contact
+    """
+    def __init__(self, context, request):
+        super(ContactActionsPanelView, self).__init__(context, request)
+        # portal_actions.object_buttons action ids to keep
+        self.ACCEPTABLE_ACTIONS = ['cut', 'copy', 'paste']
+
+
+class ContactActionsPanelViewlet(ActionsPanelViewlet):
+    """
+        Override render method for contacts
+    """
+
+    def renderViewlet(self):
+        return self.context.restrictedTraverse("@@actions_panel")(useIcons=False, showOwnDelete=True,
+                                                                  showAddContent=True, showActions=True)
