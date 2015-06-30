c42b83496330795f3416276f2e05129a70234bcf
Stephan Geulette <s.geulette@imio.be>
2015-06-30 12:19:17 +0000

    Added front-page in test

--- imio/dms/mail/testing.py
+++ imio/dms/mail/testing.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from Products.CMFPlone.utils import _createObjectByType
 from plone.testing import z2
 from plone.app.robotframework.testing import REMOTE_LIBRARY_BUNDLE_FIXTURE
 from plone.app.testing import PloneWithPackageLayer
@@ -7,15 +8,22 @@ from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
 import imio.dms.mail
 
-DMSMAIL_FIXTURE = PloneWithPackageLayer(
+
+class DmsmailLayer(PloneWithPackageLayer):
+
+    def setUpPloneSite(self, portal):
+        # we create a front-page document that will be modified in setup
+        _createObjectByType('Document', portal, id='front-page')
+        portal.setDefaultPage('front-page')
+        # install dmsmail (apply profile)
+        super(DmsmailLayer, self).setUpPloneSite(portal)
+
+DMSMAIL_FIXTURE = DmsmailLayer(
     zcml_filename="testing.zcml",
     zcml_package=imio.dms.mail,
     additional_z2_products=('Products.PythonScripts', 'imio.dms.mail', 'Products.PasswordStrength'),
     gs_profile_id='imio.dms.mail:testing',
     name="DMSMAIL_FIXTURE")
-
-#DMSMAIL_FIXTURE = PloneSandboxLayer(
-#    name="DMSMAIL_FIXTURE")
 
 DMSMAIL_INTEGRATION_TESTING = IntegrationTesting(
     bases=(DMSMAIL_FIXTURE, ),
