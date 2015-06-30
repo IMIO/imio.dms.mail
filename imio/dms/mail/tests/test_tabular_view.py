72f8157b3c700a2a528d197710902af565e997c1
Stephan Geulette <s.geulette@imio.be>
2015-06-30 10:27:37 +0000

    Added begin of test

---
+++ imio/dms/mail/tests/test_tabular_view.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import unittest2 as unittest
+from zope.component import getMultiAdapter, getUtility
+from plone.app.testing import setRoles, TEST_USER_ID, TEST_USER_NAME, login, logout
+#from plone.dexterity.utils import createContentInContainer
+from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
+
+
+class TestTabularView(unittest.TestCase):
+
+    layer = DMSMAIL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+
+    def test_render_field(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        view = self.portal.unrestrictedTraverse('incoming-mail/collections/all_mails/@@tabular_view')
