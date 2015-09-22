d6501d5ca2592e5c4c22c215218a3f0ab132761b
Stephan Geulette <s.geulette@imio.be>
2015-09-22 11:48:00 +0000

    Hide task enquirer

--- imio/dms/mail/browser/task.py
+++ imio/dms/mail/browser/task.py
@@ -1,3 +1,6 @@
+from z3c.form.interfaces import HIDDEN_MODE
+
+from plone import api
 from plone.dexterity.browser.edit import DefaultEditForm
 from plone.dexterity.browser.add import DefaultAddView
 
@@ -15,11 +18,12 @@ class TaskEdit(DefaultEditForm):
     """
       Edit view override of update
     """
-    def update(self):
-        super(TaskEdit, self).update()
+    def updateWidgets(self):
+        super(TaskEdit, self).updateWidgets()
         self.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
         self.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
         self.fields['ITask.assigned_group'].field.required = True
+        self.widgets['ITask.enquirer'].mode = HIDDEN_MODE
 
 
 class Add(DefaultAddView):
@@ -33,4 +37,8 @@ class Add(DefaultAddView):
         self.form_instance.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
         self.form_instance.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
         self.form_instance.fields['ITask.assigned_group'].field.required = True
+        userid = api.user.get_current().getId()
+        if userid != 'admin':
+            self.form_instance.fields['ITask.enquirer'].field.default = userid
+        self.form_instance.fields['ITask.enquirer'].mode = HIDDEN_MODE
         super(Add, self).update()
