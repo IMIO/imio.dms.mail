4f85b8da869807945d3fca67bcdc04da48ea8db5
Stephan Geulette <s.geulette@imio.be>
2015-09-27 17:09:09 +0000

    Added default value for assigned group

--- imio/dms/mail/browser/task.py
+++ imio/dms/mail/browser/task.py
@@ -4,6 +4,8 @@ from plone import api
 from plone.dexterity.browser.edit import DefaultEditForm
 from plone.dexterity.browser.add import DefaultAddView
 
+from Products.CMFPlone.utils import base_hasattr
+
 from ..utils import voc_selected_org_suffix_users
 
 
@@ -20,9 +22,11 @@ class TaskEdit(DefaultEditForm):
     """
     def updateWidgets(self):
         super(TaskEdit, self).updateWidgets()
+        # Override default vocabulary
         self.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
         self.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
         self.fields['ITask.assigned_group'].field.required = True
+        # Hide enquirer
         self.widgets['ITask.enquirer'].mode = HIDDEN_MODE
 
 
@@ -34,9 +38,16 @@ class Add(DefaultAddView):
 
     def update(self):
         self.form_instance.updateFields()
+        # Set parent assigned group as default value
+        if base_hasattr(self.context, 'treating_groups'):
+            self.form_instance.fields['ITask.assigned_group'].field.default = self.context.treating_groups
+        elif base_hasattr(self.context, 'assigned_group'):
+            self.form_instance.fields['ITask.assigned_group'].field.default = self.context.assigned_group
+        # Override default vocabulary
         self.form_instance.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
         self.form_instance.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
         self.form_instance.fields['ITask.assigned_group'].field.required = True
+        # Set current user as enquirer and hide it
         userid = api.user.get_current().getId()
         if userid != 'admin':
             self.form_instance.fields['ITask.enquirer'].field.default = userid
