96cdc96af98d7b12d8b8ce3d7fd07d1db0c121dd
Stephan Geulette <s.geulette@imio.be>
2015-10-02 09:06:44 +0000

    Added task im treating collection

--- imio/dms/mail/setuphandlers.py
+++ imio/dms/mail/setuphandlers.py
@@ -311,6 +311,14 @@ def createIMTaskCollections(folder):
             'flds': (u'pretty_link', u'review_state', u'assigned_group', u'assigned_user', u'due_date',
                      u'CreationDate', u'actions'),
             'sort': u'created', 'rev': True, },
+        {'id': 'im_treating', 'tit': _('task_im_treating'), 'subj': (u'todo', ), 'query': [
+            {'i': 'portal_type', 'o': 'plone.app.querystring.operation.selection.is', 'v': ['task']},
+            {'i': 'assigned_user', 'o': 'plone.app.querystring.operation.string.currentUser'},
+            {'i': 'review_state', 'o': 'plone.app.querystring.operation.selection.is', 'v': ['in_progress']}],
+            'cond': u"", 'bypass': [],
+            'flds': (u'pretty_link', u'review_state', u'assigned_group', u'assigned_user', u'due_date',
+                     u'CreationDate', u'actions'),
+            'sort': u'created', 'rev': True, },
         {'id': 'in_my_group', 'tit': _('tasks_in_my_group'), 'subj': (u'search', ), 'query': [
             {'i': 'portal_type', 'o': 'plone.app.querystring.operation.selection.is', 'v': ['task']},
             {'i': 'CompoundCriterion', 'o': 'plone.app.querystring.operation.compound.is',
