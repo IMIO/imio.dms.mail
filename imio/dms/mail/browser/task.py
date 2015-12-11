b73b18675cb974bf8577febc5926e7b365ad06f5
Stephan Geulette <s.geulette@imio.be>
2015-12-11 10:02:53 +0000

    Modify widget and not field for requirement and value, to avoid modifying globally

--- imio/dms/mail/browser/task.py
+++ imio/dms/mail/browser/task.py
@@ -1,8 +1,8 @@
 from z3c.form.interfaces import HIDDEN_MODE
 
 from plone import api
+from plone.dexterity.browser.add import DefaultAddView, DefaultAddForm
 from plone.dexterity.browser.edit import DefaultEditForm
-from plone.dexterity.browser.add import DefaultAddView
 
 from Products.CMFPlone.utils import base_hasattr
 
@@ -23,10 +23,34 @@ class TaskEdit(DefaultEditForm):
     def updateWidgets(self):
         super(TaskEdit, self).updateWidgets()
         # Override default vocabulary
+        # ERROR the modified behavior field is globally modified
         self.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
         self.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
-        self.fields['ITask.assigned_group'].field.required = True
+        # Set assigned_group as required
+        self.widgets['ITask.assigned_group'].required = True
         # Hide enquirer
+        self.widgets['ITask.enquirer'].mode = HIDDEN_MODE
+
+
+class CustomAddForm(DefaultAddForm):
+
+    portal_type = 'task'
+
+    def updateWidgets(self):
+        super(CustomAddForm, self).updateWidgets()
+        # Override default vocabulary
+        self.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
+        self.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
+        self.widgets['ITask.assigned_group'].required = True
+        # Set parent assigned group as default value
+        if base_hasattr(self.context, 'treating_groups'):
+            self.widgets['ITask.assigned_group'].value = self.context.treating_groups
+        elif base_hasattr(self.context, 'assigned_group'):
+            self.widgets['ITask.assigned_group'].value = self.context.assigned_group
+        # Set current user as enquirer and hide it
+        userid = api.user.get_current().getId()
+        if userid != 'admin':
+            self.widgets['ITask.enquirer'].value = userid
         self.widgets['ITask.enquirer'].mode = HIDDEN_MODE
 
 
@@ -34,22 +58,8 @@ class Add(DefaultAddView):
     """
         Add form redefinition to customize fields.
     """
-    portal_type = 'task'
+    form = CustomAddForm
 
-    def update(self):
-        self.form_instance.updateFields()
-        # Set parent assigned group as default value
-        if base_hasattr(self.context, 'treating_groups'):
-            self.form_instance.fields['ITask.assigned_group'].field.default = self.context.treating_groups
-        elif base_hasattr(self.context, 'assigned_group'):
-            self.form_instance.fields['ITask.assigned_group'].field.default = self.context.assigned_group
-        # Override default vocabulary
-        self.form_instance.fields['ITask.assigned_group'].field.slave_fields[0]['vocab_method'] = filter_task_assigned_users
-        self.form_instance.fields['ITask.assigned_group'].field.slave_fields[0]['initial_trigger'] = True
-        self.form_instance.fields['ITask.assigned_group'].field.required = True
-        # Set current user as enquirer and hide it
-        userid = api.user.get_current().getId()
-        if userid != 'admin':
-            self.form_instance.fields['ITask.enquirer'].field.default = userid
-        self.form_instance.fields['ITask.enquirer'].mode = HIDDEN_MODE
-        super(Add, self).update()
+#    def update(self):
+#        self.form_instance.updateFields()
+#        super(Add, self).update()
