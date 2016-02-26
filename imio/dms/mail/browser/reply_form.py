b24022d08146cec51cd53a761d887acaa39aa342
cmessiant <cmessiant@localhost>
2016-02-26 14:13:54 +0000

    Fix ReplyForm to add outgoing mail to outgoing-mail folder (ref #5318)

--- imio/dms/mail/browser/reply_form.py
+++ imio/dms/mail/browser/reply_form.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
 """Reply form."""
 # from z3c.form.interfaces import DISPLAY_MODE
+from zope.component import getUtility
 from plone.dexterity.browser.add import DefaultAddForm
+from plone import api
+from plone.dexterity.interfaces import IDexterityFTI
+from plone.dexterity.utils import addContentToContainer
 
 from imio.dms.mail import _
 
@@ -23,3 +27,18 @@ class ReplyForm(DefaultAddForm):
         self.request.form["linked_mails"] = value
         self.request.form["form.widgets.linked_mails"] = value
         # self.fields['linked_mails'].mode = DISPLAY_MODE
+
+    def add(self, obj):
+        """Create outgoing mail in outgoing-mail folder."""
+        fti = getUtility(IDexterityFTI, name=self.portal_type)
+        container = api.portal.get()['outgoing-mail']
+        new_object = addContentToContainer(container, obj)
+
+        if fti.immediate_view:
+            self.immediate_view = "/".join(
+                [container.absolute_url(), new_object.id, fti.immediate_view]
+            )
+        else:
+            self.immediate_view = "/".join(
+                [container.absolute_url(), new_object.id]
+            )
