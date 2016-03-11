83eacab19419a71856a7cf4ab9f9842b9f086cd7
Stephan Geulette <s.geulette@imio.be>
2016-03-11 22:20:22 +0000

    Added test on actionspanel

---
+++ imio/dms/mail/tests/test_actionspanel.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""Test views."""
+import unittest
+from plone import api
+from plone.app.testing import setRoles, TEST_USER_ID
+
+from ..testing import DMSMAIL_INTEGRATION_TESTING
+
+
+class TestDmsIMActionsPanelView(unittest.TestCase):
+
+    layer = DMSMAIL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.im1 = self.portal['incoming-mail']['courrier1']
+        self.view = self.im1.unrestrictedTraverse('@@actions_panel')
+
+    def test_renderReplyButton(self):
+        self.assertEqual(self.view.renderReplyButton(), "")
+
+    def test_sortTransitions(self):
+        self.assertListEqual([t['id'] for t in self.view.getTransitions()],
+                             ['propose_to_manager', 'propose_to_service_chief'])
+        api.content.transition(obj=self.im1, to_state='proposed_to_agent')
+        self.assertListEqual([t['id'] for t in self.view.getTransitions()],
+                             ['back_to_service_chief', 'treat', 'close'])
+
+
+class TestContactActionsPanelView(unittest.TestCase):
+
+    layer = DMSMAIL_INTEGRATION_TESTING
+
+    def test_init(self):
+        self.portal = self.layer['portal']
+        self.swde = self.portal['contacts']['swde']
+        self.view = self.swde.unrestrictedTraverse('@@actions_panel')
