292b89e4e1d6bc6b2bc478451295907c672244ea
Stephan Geulette <s.geulette@imio.be>
2016-05-26 14:29:34 +0000

    Corrected test on version column

--- imio/dms/mail/tests/test_table.py
+++ imio/dms/mail/tests/test_table.py
@@ -6,7 +6,7 @@ from plone.dexterity.utils import createContentInContainer
 from plone.registry.interfaces import IRegistry
 from collective.contact.plonegroup.config import ORGANIZATIONS_REGISTRY
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
-from imio.dms.mail.browser.table import VersionsTitleColumn, AssignedGroupColumn
+from imio.dms.mail.browser.table import IMVersionsTitleColumn, AssignedGroupColumn
 
 
 class TestTable(unittest.TestCase):
@@ -26,8 +26,8 @@ class TestTable(unittest.TestCase):
         # Cannot use scan_date because toLocalizedTime causes error in test
         brains = self.portal.portal_catalog(portal_type='dmsmainfile', id='testid1')
         self.assertEqual(len(brains), 1)
-        col = VersionsTitleColumn(self.portal, self.portal.REQUEST, None)
-        self.assertEqual(col.getLinkTitle(brains[0]), u'title="scan_id: 123456789\nscan_date: "')
+        col = IMVersionsTitleColumn(self.portal, self.portal.REQUEST, None)
+        self.assertEqual(col.getLinkTitle(brains[0]), u'title="scan_id: 123456789\nscan_date: \nVersion: "')
 
     def test_AssignedGroupColumn(self):
         registry = getUtility(IRegistry)
