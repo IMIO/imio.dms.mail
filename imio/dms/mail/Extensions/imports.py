556d717c1f75671650c5bbcb0dc7632454ed9aa3
Stephan Geulette <s.geulette@imio.be>
2016-09-19 14:51:30 +0000

    Added imports method

--- imio/dms/mail/Extensions/imports.py
+++ imio/dms/mail/Extensions/imports.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 from collections import OrderedDict
 
 
@@ -12,3 +13,19 @@ def change_levels(cid, childs, orgs):
 
 def sort_by_level(orgs):
     return OrderedDict(sorted(orgs.items(), key=lambda item: (item[1]['lev'], item[1]['tit'])))
+
+
+def assert_value_in_list(val, lst):
+    assert val in lst, "Value '%s' not in valid values '%s'" % (val, lst)
+    return val
+
+
+def assert_date(val, fmt='%Y/%m/%d', can_be_empty=True):
+    if not val and can_be_empty:
+        return None
+    try:
+        dtm = datetime.datetime.strptime(val, fmt)
+        dt = dtm.date()
+    except Exception, ex:
+        raise AssertionError(u"Not a valid date '%s': %s" % (val, ex))
+    return dt
