53642ebf88f74a3cb3cc91a357e853d9d8b06d2f
Stephan Geulette <s.geulette@imio.be>
2017-02-17 12:22:03 +0000

    Update index

--- imio/dms/mail/migrations/migrate_to_2_0.py
+++ imio/dms/mail/migrations/migrate_to_2_0.py
@@ -199,6 +199,7 @@ class Migrate_To_2_0(Migrator):
             obj.reindexObject(idxs=['in_out_date'])
             obj.reindexObjectSecurity()
         # self.upgradeAll()
+        self.catalog.reindexIndex(['assigned_user'], self.portal.REQUEST)
 
         self.runProfileSteps('imio.dms.mail', steps=['cssregistry', 'jsregistry'])
 
