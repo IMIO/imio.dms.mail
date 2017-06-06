328657370c1ea3d13c5b00c2fec717c0d7d070a4
Stephan Geulette <s.geulette@imio.be>
2017-06-06 09:36:39 +0000

    Resolved bug in reply form #17614

--- imio/dms/mail/browser/reply_form.py
+++ imio/dms/mail/browser/reply_form.py
@@ -8,8 +8,6 @@ from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import addContentToContainer
 from Products.CMFPlone.utils import safe_unicode
 
-from Products.CPUtils.Extensions.utils import safe_encode
-
 from imio.dms.mail import _
 from ..dmsmail import ImioDmsOutgoingMailUpdateFields, ImioDmsOutgoingMailUpdateWidgets
 
@@ -26,21 +24,32 @@ class ReplyForm(DefaultAddForm):
         return _(u"Reply to ${ref}", mapping={'ref': safe_unicode(self.context.Title())})
 
     def updateFields(self):
+
         super(ReplyForm, self).updateFields()
         imail = self.context
         form = self.request.form
-        form["form.widgets.IDublinCore.title"] = "Réponse: %s" % safe_encode(imail.title)
-        form["form.widgets.treating_groups"] = imail.treating_groups
+        # Completing form values wasn't working anymore, but relations must be set here too !
         form["form.widgets.reply_to"] = ('/'.join(imail.getPhysicalPath()),)
-        form["form.widgets.recipients"] = ('/'.join(imail.sender.to_object.getPhysicalPath()), )
-        if imail.external_reference_no:
-            form["form.widgets.external_reference_no"] = imail.external_reference_no
-        if imail.recipient_groups:
-            form["form.widgets.recipient_groups"] = imail.recipient_groups
+        form["form.widgets.recipients"] = (imail.sender.to_path, )
+        # form["form.widgets.IDublinCore.title"] = "Réponse: %s" % safe_encode(imail.title)
+        # form["form.widgets.treating_groups"] = imail.treating_groups
+        # if imail.external_reference_no:
+        #     form["form.widgets.external_reference_no"] = imail.external_reference_no
+        # if imail.recipient_groups:
+        #     form["form.widgets.recipient_groups"] = imail.recipient_groups
         ImioDmsOutgoingMailUpdateFields(self)
 
     def updateWidgets(self):
         super(ReplyForm, self).updateWidgets()
+        imail = self.context
+        self.widgets["IDublinCore.title"].value = u"Réponse: %s" % safe_unicode(imail.title)
+        self.widgets["treating_groups"].value = imail.treating_groups
+        self.widgets["reply_to"].value = ('/'.join(imail.getPhysicalPath()),)
+        self.widgets["recipients"].value = (imail.sender.to_path, )
+        if imail.external_reference_no:
+            self.widgets["external_reference_no"].value = imail.external_reference_no
+        if imail.recipient_groups:
+            self.widgets["recipient_groups"].value = imail.recipient_groups
         ImioDmsOutgoingMailUpdateWidgets(self)
 
     def add(self, obj):
