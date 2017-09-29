82b6eb9773cdb2bb86d649a4ce756dc169f4f486
Stephan Geulette <s.geulette@imio.be>
2017-09-29 09:29:41 +0000

    Added script to clean all examples and demo configuration

--- imio/dms/mail/Extensions/demo.py
+++ imio/dms/mail/Extensions/demo.py
@@ -9,7 +9,9 @@ from collective.dms.mailcontent.dmsmail import (internalReferenceIncomingMailDef
                                                 internalReferenceOutgoingMailDefaultValue)
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.utils import safe_unicode
+from Products.CPUtils.Extensions.utils import check_zope_admin, log_list
 from datetime import datetime
+from collective.contact.plonegroup.config import ORGANIZATIONS_REGISTRY, FUNCTIONS_REGISTRY
 from imio.dms.mail import add_path
 
 
@@ -140,3 +142,82 @@ def create_main_file(self, filename='', title='1', mainfile_type='dmsmainfile'):
         file_object = NamedBlobFile(fo.read(), filename=safe_unicode(filename))
         obj = createContentInContainer(self, mainfile_type, title=safe_unicode(title), file=file_object)
     return obj.REQUEST.response.redirect('%s/view' % obj.absolute_url())
+
+
+def clean_examples(self):
+    """ Clean created examples """
+    if not check_zope_admin():
+        return "You must be a zope manager to run this script"
+    out = []
+    portal = api.portal.getSite()
+    # Delete om
+    brains = api.content.find(portal_type='dmsoutgoingmail')
+    for brain in brains:
+        log_list(out, "Deleting om '%s'" % brain.getPath())
+        api.content.delete(obj=brain.getObject(), check_linkintegrity=True)
+    # Delete im
+    brains = api.content.find(portal_type='dmsincomingmail')
+    for brain in brains:
+        log_list(out, "Deleting im '%s'" % brain.getPath())
+        api.content.delete(obj=brain.getObject(), check_linkintegrity=True)
+    # Delete own personnel
+    pf = portal['contacts']['personnel-folder']
+    brains = api.content.find(context=pf, portal_type='person')
+    for brain in brains:
+        log_list(out, "Deleting person '%s'" % brain.getPath())
+        api.content.delete(obj=brain.getObject(), check_linkintegrity=True)
+    # Deactivate own organizations
+    ownorg = portal['contacts']['plonegroup-organization']
+    brains = api.content.find(context=ownorg, portal_type='organization',
+                              id=['plonegroup-organization', 'college-communal', 'conseil-communal'])
+    kept_orgs = [brain.UID for brain in brains]
+    log_list(out, "Activating only 'college-communal'")
+    api.portal.set_registry_record(name=ORGANIZATIONS_REGISTRY, value=[ownorg['college-communal'].UID()])
+    # Delete organization and template folders
+    tmpl_folder = portal['templates']['om']
+    brains = api.content.find(context=ownorg, portal_type='organization', sort_on='path', sort_order='descending')
+    for brain in brains:
+        uid = brain.UID
+        if uid in kept_orgs:
+            continue
+        log_list(out, "Deleting organization '%s'" % brain.getPath())
+        api.content.delete(obj=brain.getObject(), check_linkintegrity=True)
+        if uid in tmpl_folder:
+            log_list(out, "Deleting template folder '%s'" % '/'.join(tmpl_folder[uid].getPhysicalPath()))
+            api.content.delete(obj=tmpl_folder[uid])
+    # Delete users
+    for userid in ['encodeur', 'dirg', 'chef', 'agent', 'agent1', 'lecteur']:
+        user = api.user.get(userid=userid)
+        for group in api.group.get_groups(user=user):
+            if group.id == 'AuthenticatedUsers':
+                continue
+            log_list(out, "Removing user '%s' from group '%s'" % (userid, group.getProperty('title')))
+            api.group.remove_user(group=group, user=user)
+        log_list(out, "Deleting user '%s'" % userid)
+        api.user.delete(user=user)
+    # Delete groups
+    functions = [dic['fct_id'] for dic in api.portal.get_registry_record(FUNCTIONS_REGISTRY)]
+    groups = api.group.get_groups()
+    for group in groups:
+        if '_' not in group.id or group.id in ['dir_general']:
+            continue
+        parts = group.id.split('_')
+        if len(parts) == 1:
+            continue
+        org_uid = parts[0]
+        function = '_'.join(parts[1:])
+        if org_uid in kept_orgs or function not in functions:
+            continue
+        log_list(out, "Deleting group '%s'" % group.getProperty('title'))
+        api.group.delete(group=group)
+    # Delete contacts
+    brains = api.content.find(context=portal['contacts'], portal_type='person',
+                              id=['jeancourant', 'sergerobinet', 'bernardlermitte'])
+    for brain in brains:
+        log_list(out, "Deleting person '%s'" % brain.getPath())
+        api.content.delete(obj=brain.getObject(), check_linkintegrity=True)
+    brains = api.content.find(context=portal['contacts'], portal_type='organization', id=['electrabel', 'swde'])
+    for brain in brains:
+        log_list(out, "Deleting organization '%s'" % brain.getPath())
+        api.content.delete(obj=brain.getObject(), check_linkintegrity=True)
+    return '\n'.join(out)
