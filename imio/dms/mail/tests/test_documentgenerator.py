b7ce2e9315af47029c2d19c1cfdb6e421aade2b5
Stephan Geulette <s.geulette@imio.be>
2018-02-16 15:47:50 +0000

    Improved coverage

--- imio/dms/mail/tests/test_documentgenerator.py
+++ imio/dms/mail/tests/test_documentgenerator.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 """ documentgenerator.py tests for this package."""
+import mocker
 import unittest
 
+from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
-from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
+from z3c.relationfield.relation import RelationValue
+from zope.component import getUtility
+from zope.intid.interfaces import IIntIds
 
 
 class TestDocumentGenerator(unittest.TestCase):
@@ -16,27 +20,166 @@ class TestDocumentGenerator(unittest.TestCase):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.pc = self.portal.portal_catalog
+        self.intids = getUtility(IIntIds)
         self.omf = self.portal['outgoing-mail']
+        self.ctct = self.portal['contacts']
+        self.electrabel = self.ctct['electrabel']
+        self.jc = self.ctct['jeancourant']
+        self.agent = self.jc['agent-electrabel']
+        self.grh = self.ctct['plonegroup-organization']['direction-generale']['grh']
+        self.chef = self.ctct['personnel-folder']['chef']
+        self.resp_grh = self.chef['responsable-grh']
+
+    def test_OMDGHelper(self):
+        view1 = self.omf.reponse1.unrestrictedTraverse('@@document_generation_helper_view')
+
+        # Test fmt method
+        self.assertEqual(view1.fmt(None), '')
+        self.assertEqual(view1.fmt('Test'), 'Test ')
+        self.assertEqual(view1.fmt('Test', fmt='(%s)'), '(Test)')
+
+        # Test get_ctct_det method
+        self.assertDictEqual(view1.get_ctct_det(''), {})
+        det = {'address': {'city': u'E-ville', 'country': '', 'region': '',
+                           'additional_address_details': '', 'number': u'1',
+                           'street': u"Rue de l'électron", 'zip_code': u'0020'},
+               'website': '', 'fax': '', 'phone': u'012/345.678', 'im_handle': '',
+               'cell_phone': '', 'email': u'jean.courant@electrabel.be'}
+        self.assertDictEqual(view1.get_ctct_det(self.jc), det)
+
+        # Test get_sender method
+        sender = {'person': self.chef, 'hp': self.resp_grh, 'org_full_title': u'Direction générale - GRH',
+                  'org': self.grh}
+        self.assertDictEqual(view1.get_sender(), sender)
+        backup = view1.real_context.sender
+        view1.real_context.sender = ''
+        self.assertDictEqual(view1.get_sender(), {})
+        view1.real_context.sender = backup
+
+        # Test mailing_list method
+        self.assertListEqual(view1.mailing_list(), [self.ctct['electrabel']])
+        view1.real_context.recipients.append(RelationValue(self.intids.getId(self.electrabel)))
+        self.assertListEqual(view1.mailing_list(), [self.ctct['electrabel'], self.electrabel])
+        backup = view1.real_context.recipients[0]
+        view1.real_context.recipients = None
+        self.assertListEqual(view1.mailing_list(), [])
+        view1.real_context.recipients = [backup]
+
+        # Test get_full_title method
+        self.assertEqual(view1.get_full_title(None), '')
+        self.assertEqual(view1.get_full_title(self.electrabel), u'Electrabel')
+        self.assertEqual(view1.get_full_title(self.grh), u'Mon organisation / Direction générale / GRH')
+        self.assertEqual(view1.get_full_title(self.grh, separator=' - ', first_index=1), u'Direction générale - GRH')
+        self.assertEqual(view1.get_full_title(self.jc), u'Monsieur Jean Courant')
+        self.assertEqual(view1.get_full_title(self.agent), u'Monsieur Jean Courant (Electrabel - Agent)')
+
+        # Test get_separate_titles method
+        self.assertListEqual(view1.get_separate_titles(None), [u'', u''])
+        self.assertListEqual(view1.get_separate_titles(self.electrabel), [u'Electrabel', u''])
+        self.assertListEqual(view1.get_separate_titles(self.grh), [u'Mon organisation / Direction générale / GRH', ''])
+        self.assertListEqual(view1.get_separate_titles(self.grh, separator=' - ', first_index=1),
+                             [u'Direction générale - GRH', ''])
+        self.assertListEqual(view1.get_separate_titles(self.jc), ['', u'Monsieur Jean Courant'])
+        self.assertListEqual(view1.get_separate_titles(self.agent), [u'Electrabel', u'Monsieur Jean Courant'])
+        self.assertListEqual(view1.get_separate_titles(self.resp_grh),
+                             [u'Mon organisation / Direction générale / GRH', u'Monsieur Michel Chef'])
+
+        # Test person_title
+        self.assertEqual(view1.person_title(None), '')
+        self.assertEqual(view1.person_title(self.jc), u'Monsieur')
+        self.jc.person_title = None
+        self.assertEqual(view1.person_title(self.jc), u'Monsieur')
+        self.assertEqual(view1.person_title(self.jc, pers_dft=u'Madame'), u'Madame')
+        self.assertEqual(view1.person_title(self.electrabel), u'Madame, Monsieur')
+        self.assertEqual(view1.person_title(self.electrabel, org_dft=u'Messieurs'), u'Messieurs')
+        self.assertEqual(view1.person_title(self.agent), u'Monsieur')
+
+        # Test is_first_doc
+        mock = mocker.Mocker()
+        res = {}
+        view1.appy_renderer = mock.mock()
+        mocker.expect(view1.appy_renderer.contentParser.env.context).result(res).replay()
+        self.assertTrue(view1.is_first_doc())
+        mock2 = mocker.Mocker()
+        res['loop'] = mock2.mock()
+        mocker.expect(res['loop'].mailed_data.first).result(False).replay()
+        mock.replay()
+        self.assertFalse(view1.is_first_doc())
 
     def test_DocumentGenerationOMDashboardHelper(self):
         view = self.omf['mail-searches'].unrestrictedTraverse('@@document_generation_helper_view')
-        # for dashboard
+
+        # Test is_dashboard
         view.request.form['facetedQuery'] = ''
         self.assertTrue(view.is_dashboard())
-        brains = self.pc(id=['reponse1', 'reponse2', 'reponse3'])
+
+        # Test uids_to_objs
+        brains = self.pc(id=['reponse1', 'reponse2', 'reponse3'], sort_on='id')
+        self.assertEqual(len(view.objs), 0)
+        view.uids_to_objs(brains)
+        self.assertEqual(len(view.objs), 3)
+
+        # Test group_by_tg
+        tg1 = self.ctct['plonegroup-organization']['direction-generale']
+        tg2 = tg1[u'secretariat']
+        res = {tg1.UID(): {'mails': [view.objs[0]], 'title': u'Direction générale'},
+               tg2.UID(): {'mails': [view.objs[1]], 'title': u'Direction générale - Secrétariat'}}
+        self.assertDictEqual(view.group_by_tg(brains[:2]), res)
+        backup = brains[1].treating_groups
+        brains[1].treating_groups = None
+        res = {tg1.UID(): {'mails': [view.objs[0]], 'title': u'Direction générale'},
+               '1_no_group': {'mails': [view.objs[1]], 'title': u'No treating group'}}
+        self.assertDictEqual(view.group_by_tg(brains[:2]), res)
+        brains[1].treating_groups = backup
+
+        # Test get_dms_files
         view.context_var = lambda x: brains
-        objs = [b.getObject() for b in brains]
-        # test getting files
-        files = view.get_dms_files()
-        self.assertListEqual(files, [objs[0]['1'], objs[1]['1'], objs[2]['1']])
-        # self.assertListEqual(files, [(objs[0]['1'], False, True), (objs[1]['1'], True, False),
-        #                              (objs[2]['1'], False, False)])
-        # test getting num pages
-        self.assertEquals(view.get_num_pages(objs[0]['1']), 1)
-        self.assertEquals(view.get_num_pages(objs[1]['1']), 2)
-        self.assertEquals(view.get_num_pages(objs[2]['1']), 1)
-        # test getting images
-        images = view.get_dv_images(objs[0]['1'])
+        files = view.get_dms_files(limit=1)
+        self.assertListEqual(files, [view.objs[0]['1'], view.objs[1]['1'], view.objs[2]['1']])
+        del view.request.form['facetedQuery']
+        self.assertListEqual(view.get_dms_files(), [])
+
+        # Test get_num_pages
+        self.assertEquals(view.get_num_pages(view.objs[0]['1']), 1)
+        self.assertEquals(view.get_num_pages(view.objs[1]['1']), 2)
+        self.assertEquals(view.get_num_pages(view.objs[2]['1']), 1)
+        self.assertEquals(view.get_num_pages(self.portal['incoming-mail']), 0)
+
+        # Test get_dv_images
+        images = view.get_dv_images(view.objs[0]['1'])
         self.assertEqual(len(images), 1)
         self.assertTrue(hasattr(images[0], 'read'))
         images[0].close()
+
+    def test_DocumentGenerationDirectoryHelper(self):
+        view = self.ctct.unrestrictedTraverse('@@document_generation_helper_view')
+        # Test get_organisations
+        res = [
+            (1, '', self.ctct['electrabel']),
+            (2, 1, self.ctct['electrabel']['travaux']),
+            (3, '', self.ctct['plonegroup-organization']),
+            (4, 3, self.ctct['plonegroup-organization']['college-communal'])]
+        self.assertListEqual(view.get_organizations()[:4], res)
+
+        # Test get_persons
+        res = [
+            (1, self.ctct['personnel-folder']['agent']), (2, self.chef),
+            (3, self.ctct['jeancourant']), (4, self.ctct['personnel-folder']['dirg']),
+            (5, self.ctct['bernardlermitte']), (6, self.ctct['notencoded']),
+            (7, self.ctct['sergerobinet'])]
+        self.assertListEqual(view.get_persons(), res)
+
+        # Test get_held_positions
+        res = [
+            (1, 5, 27, self.ctct['bernardlermitte']['agent-swde']),
+            (2, 3, 1, self.ctct['jeancourant']['agent-electrabel']),
+            (3, 1, 19, self.ctct['personnel-folder']['agent']['agent-grh'])]
+        self.assertListEqual(view.get_held_positions()[:3], res)
+
+
+
+
+
+
+
+
--- setup.py
+++ setup.py
@@ -15,13 +15,14 @@ long_description = (
     open('CHANGES.rst').read()
     + '\n')
 
-setup(name='imio.dms.mail',
-      version=version,
-      description="Imio dms mail",
-      long_description=long_description,
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
+setup(
+    name='imio.dms.mail',
+    version=version,
+    description="Imio dms mail",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
         "Framework :: Plone :: 4.2",
@@ -30,60 +31,61 @@ setup(name='imio.dms.mail',
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Topic :: Software Development :: Libraries :: Python Modules",
-        ],
-      keywords='',
-      author='',
-      author_email='',
-      url='http://svn.communesplone.org/svn/communesplone/imio.dms.mail',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['imio', 'imio.dms'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'Plone',
-          'Products.CPUtils',
-          'Products.PasswordStrength',
-          'collective.ckeditor',
-          'collective.contact.core',
-          'collective.contact.duplicated',
-          'collective.contact.facetednav',
-          'collective.contact.plonegroup',
-          'collective.dexteritytextindexer',
-          'collective.dms.basecontent',
-          'collective.dms.batchimport',
-          'collective.dms.mailcontent',
-          'collective.dms.scanbehavior',
-          'collective.documentgenerator',
-          'collective.externaleditor',
-          'collective.iconifieddocumentactions',
-          'collective.js.fancytree',
-          'collective.messagesviewlet',
-          'collective.querynextprev',
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://svn.communesplone.org/svn/communesplone/imio.dms.mail',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['imio', 'imio.dms'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'Plone',
+        'Products.CPUtils',
+        'Products.PasswordStrength',
+        'collective.ckeditor',
+        'collective.contact.core',
+        'collective.contact.duplicated',
+        'collective.contact.facetednav',
+        'collective.contact.plonegroup',
+        'collective.dexteritytextindexer',
+        'collective.dms.basecontent',
+        'collective.dms.batchimport',
+        'collective.dms.mailcontent',
+        'collective.dms.scanbehavior',
+        'collective.documentgenerator',
+        'collective.externaleditor',
+        'collective.iconifieddocumentactions',
+        'collective.js.fancytree',
+        'collective.messagesviewlet',
+        'collective.querynextprev',
 #          'collective.schedulefield',
-          'collective.task',
-          'collective.wfadaptations',
-          'communesplone.layout',
-          'dexterity.localrolesfield',
-          'imio.dashboard',
-          'imio.helpers',
-          'imio.migrator',
-          'imio.zamqp.core',
-          'plone.app.dexterity[grok]',
-          'plone.app.lockingbehavior',
-          'plonetheme.imioapps',
-          'z3c.unconfigure',
-          # -*- Extra requirements: -*-
-      ],
-      extras_require={'test':
-        ['plone.app.testing',
-         'plone.app.robotframework',
-         'plonetheme.imioapps',]},
-      entry_points="""
-      # -*- Entry points: -*-
+        'collective.task',
+        'collective.wfadaptations',
+        'communesplone.layout',
+        'dexterity.localrolesfield',
+        'imio.dashboard',
+        'imio.helpers',
+        'imio.migrator',
+        'imio.zamqp.core',
+        'plone.app.dexterity[grok]',
+        'plone.app.lockingbehavior',
+        'plonetheme.imioapps',
+        'z3c.unconfigure',
+        # -*- Extra requirements: -*-
+    ],
+    extras_require={'test': [
+        'plone.app.testing',
+        'plone.app.robotframework',
+        'plone.mocktestcase',
+        'plonetheme.imioapps']},
+    entry_points="""
+    # -*- Entry points: -*-
 
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
