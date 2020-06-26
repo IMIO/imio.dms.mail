634715f849040d95306a47d857fbd1d2490970d1
Benoit Leybaert <benoit.leybaert@imio.be>
2020-06-26 12:19:55 +0200

    Fix local roles test if serfvice_chief was removed in a wf adaptation

--- imio/dms/mail/migrations/migrate_to_3_0.py
+++ imio/dms/mail/migrations/migrate_to_3_0.py
@@ -61,13 +61,14 @@ class Migrate_To_3_0(Migrator):
         lrsc = lr['static_config']
         for state in ['proposed_to_manager', 'proposed_to_service_chief',
                       'proposed_to_agent', 'in_treatment', 'closed']:
-            if 'lecteurs_globaux_ce' not in lrsc[state]:
-                lrsc[state]['lecteurs_globaux_ce'] = {'roles': ['Reader']}
+            if state in lrsc:
+                if 'lecteurs_globaux_ce' not in lrsc[state]:
+                    lrsc[state]['lecteurs_globaux_ce'] = {'roles': ['Reader']}
         # We need to indicate that the object has been modified and must be "saved"
         lr._p_changed = True
 
-        if 'new-version' not in self.portal['messages-config']:
-            'TO BE CONTINUED' / 1
+        # if 'new-version' not in self.portal['messages-config']:
+        #    'TO BE CONTINUED' / 1
 
     def insert_incoming_emails(self):
         # allowed types
