7ab78d16be63d0d0df61db834d473da8b38bcbab
Stephan Geulette <s.geulette@imio.be>
2020-08-24 13:55:11 +0200

    Added imservicevalidation tests for n+1 and n+2 levels

--- imio/dms/mail/tests/test_wfadaptations_imservicevalidation.py
+++ imio/dms/mail/tests/test_wfadaptations_imservicevalidation.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 """ wfadaptations.py tests for this package."""
-from imio.dms.mail.browser.settings import IImioDmsMailConfig
+from collective.contact.plonegroup.config import get_registry_functions
+from collective.contact.plonegroup.config import get_registry_organizations
+from collective.wfadaptations.api import add_applied_adaptation
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
+from imio.dms.mail.utils import get_dms_config
 from imio.dms.mail.utils import IdmUtilsMethods
-from imio.dms.mail.wfadaptations import OMToPrintAdaptation
-from imio.dms.mail.wfadaptations import IMSkipProposeToServiceChief
-from imio.dms.mail.wfadaptations import OMSkipProposeToServiceChief
 from imio.dms.mail.wfadaptations import IMServiceValidation
 from plone import api
 from plone.app.testing import login
@@ -16,8 +16,10 @@ from zope.component import getUtility
 
 import unittest
 
+from zope.schema.interfaces import IVocabularyFactory
 
-class TestWFAdaptations(unittest.TestCase):
+
+class TestIMServiceValidation1(unittest.TestCase):
 
     layer = DMSMAIL_INTEGRATION_TESTING
 
@@ -25,43 +27,75 @@ class TestWFAdaptations(unittest.TestCase):
         self.portal = self.layer['portal']
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
         self.pw = self.portal.portal_workflow
+        self.imw = self.pw['incomingmail_workflow']
         api.group.create('abc_group_encoder', 'ABC group encoder')
         self.portal.portal_setup.runImportStepFromProfile('profile-imio.dms.mail:singles',
                                                           'imiodmsmail-apply_n_plus_1_wfadaptation',
                                                           run_dependencies=False)
 
-# TODO states vocabulary,
+    def test_im_workflow1(self):
+        """ Check workflow """
+        self.assertSetEqual(set(self.imw.states), set(['created', 'proposed_to_manager', 'proposed_to_n_plus_1',
+                                                       'proposed_to_agent', 'in_treatment', 'closed']))
+        self.assertSetEqual(set(self.imw.transitions),
+                            set(['back_to_creation', 'back_to_manager', 'back_to_n_plus_1', 'back_to_agent',
+                                 'back_to_treatment', 'propose_to_manager', 'propose_to_n_plus_1', 'propose_to_agent',
+                                 'treat', 'close']))
+        self.assertSetEqual(set(self.imw.states['created'].transitions),
+                            set(['propose_to_manager', 'propose_to_n_plus_1', 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_manager'].transitions),
+                            set(['back_to_creation', 'propose_to_n_plus_1', 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_n_plus_1'].transitions),
+                            set(['back_to_creation', 'back_to_manager', 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_agent'].transitions),
+                            set(['back_to_creation', 'back_to_manager', 'back_to_n_plus_1', 'treat', 'close']))
+        self.assertSetEqual(set(self.imw.states['in_treatment'].transitions),
+                            set(['back_to_agent', 'close']))
+        self.assertSetEqual(set(self.imw.states['closed'].transitions),
+                            set(['back_to_treatment', 'back_to_agent']))
 
-    def test_workflow(self):
-        """ """
-        pass
-
-    def test_IMServiceValidation(self):
+    def test_IMServiceValidation1(self):
         """
-            Test all methods of IMServiceValidation class
+            Test IMServiceValidation adaptations
         """
-        imsv = IMServiceValidation()
-        im_workflow = self.pw['incomingmail_workflow']
-        self.assertEqual(imsv.check_state_in_workflow(im_workflow, 'proposed_to_n_plus_1'), '')
-        self.assertNotEqual(imsv.check_state_in_workflow(im_workflow, 'proposed_to_n_plus_2'), '')
-        imsv.patch_workflow('incomingmail_workflow', validation_level=2,
-                            state_title=u'Valider par le chef de département',
-                            forward_transition_title=u'Proposer au chef de département',
-                            backward_transition_title=u'Renvoyer au chef de département',
-                            function_title=u'chef de département')
-        self.assertEqual(imsv.check_state_in_workflow(im_workflow, 'proposed_to_n_plus_2'), '')
-        self.assertEqual(imsv.check_transition_in_workflow(im_workflow, 'propose_to_n_plus_2'), '')
-        self.assertEqual(imsv.check_transition_in_workflow(im_workflow, 'back_to_n_plus_2'), '')
+        # is function added
+        self.assertIn('n_plus_1', [fct['fct_id'] for fct in get_registry_functions()])
+        # is local roles modified
         fti = getUtility(IDexterityFTI, name='dmsincomingmail')
         lr = getattr(fti, 'localroles')
-        self.assertIn('proposed_to_n_plus_2', lr['static_config'])
-        self.assertIn('proposed_to_n_plus_2', lr['treating_groups'])
-        self.assertIn('proposed_to_n_plus_2', lr['recipient_groups'])
-        self.assertTrue(self.portal['incoming-mail']['mail-searches']['searchfor_proposed_to_n_plus_2'].enabled)
+        self.assertIn('proposed_to_n_plus_1', lr['static_config'])
+        self.assertIn('proposed_to_n_plus_1', lr['treating_groups'])
+        self.assertIn('proposed_to_n_plus_1', lr['recipient_groups'])
+        # check collection
+        folder = self.portal['incoming-mail']['mail-searches']
+        self.assertIn('searchfor_proposed_to_n_plus_1', folder)
+        self.assertEqual(folder.getObjectPosition('searchfor_proposed_to_agent'), 13)
+        self.assertEqual(folder.getObjectPosition('searchfor_proposed_to_n_plus_1'), 12)
+        self.assertFalse(folder['to_treat_in_my_group'].showNumberOfItems)
+        # check annotations
+        config = get_dms_config(['review_levels', 'dmsincomingmail'])
+        self.assertIn('_n_plus_1', config)
+        config = get_dms_config(['review_states', 'dmsincomingmail'])
+        self.assertIn('proposed_to_n_plus_1', config)
+        config = get_dms_config(['transitions_auc', 'dmsincomingmail'])
+        self.assertIn('propose_to_n_plus_1', config)
+        config = get_dms_config(['transitions_levels', 'dmsincomingmail'])
+        self.assertEqual(config['proposed_to_manager'].values()[0][0], 'propose_to_n_plus_1')
+        self.assertEqual(config['proposed_to_agent'].values()[0][1], 'back_to_n_plus_1')
+        # check vocabularies
+        factory = getUtility(IVocabularyFactory, u'collective.eeafaceted.collectionwidget.cachedcollectionvocabulary')
+        self.assertEqual(len(factory(folder, folder)), 16)
+        factory = getUtility(IVocabularyFactory, u'imio.dms.mail.IMReviewStatesVocabulary')
+        self.assertEqual(len(factory(folder)), 6)
+        # check configuration
+        lst = api.portal.get_registry_record('imio.actionspanel.browser.registry.IImioActionsPanelConfig.transitions')
+        self.assertIn('dmsincomingmail.back_to_n_plus_1|', lst)
+        lst = api.portal.get_registry_record('imio.dms.mail.browser.settings.IImioDmsMailConfig.imail_remark_states')
+        self.assertIn('proposed_to_n_plus_1', lst)
 
-    def test_IdmUtilsMethods_proposed_to_n_plus_col_cond(self):
-        im_folder = self.portal['incoming-mail']['mail-searches']
-        col = im_folder['searchfor_proposed_to_n_plus_1']
+    def test_IdmUtilsMethods_proposed_to_n_plus_col_cond1(self):
+        folder = self.portal['incoming-mail']['mail-searches']
+        col = folder['searchfor_proposed_to_n_plus_1']
         n_plus_1_view = IdmUtilsMethods(col, col.REQUEST)
         self.assertFalse(n_plus_1_view.proposed_to_n_plus_col_cond())
         login(self.portal, 'encodeur')
@@ -75,23 +109,125 @@ class TestWFAdaptations(unittest.TestCase):
         self.assertTrue(n_plus_1_view.proposed_to_n_plus_col_cond())
         login(self.portal, 'chef')
         self.assertTrue(n_plus_1_view.proposed_to_n_plus_col_cond())
-        # create N+2 validation by patching the workflow
-        login(self.portal, 'test-user')
+
+
+class TestIMServiceValidation2(unittest.TestCase):
+
+    layer = DMSMAIL_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.pw = self.portal.portal_workflow
+        self.imw = self.pw['incomingmail_workflow']
+        api.group.create('abc_group_encoder', 'ABC group encoder')
+        self.portal.portal_setup.runImportStepFromProfile('profile-imio.dms.mail:singles',
+                                                          'imiodmsmail-apply_n_plus_1_wfadaptation',
+                                                          run_dependencies=False)
         sva = IMServiceValidation()
-        sva.patch_workflow('incomingmail_workflow',
-                           validation_level=2,
-                           state_title=u'Valider par le chef de département',
-                           forward_transition_title=u'Proposer au chef de département',
-                           backward_transition_title=u'Renvoyer au chef de département',
-                           function_title=u'chef de département')
-        col = im_folder['searchfor_proposed_to_n_plus_2']
-        n_plus_2_view = IdmUtilsMethods(col, col.REQUEST)
+        n_plus_2_params = {'validation_level': 2,
+                           'state_title': u'Valider par le chef de département',
+                           'forward_transition_title': u'Proposer au chef de département',
+                           'backward_transition_title': u'Renvoyer au chef de département',
+                           'function_title': u'chef de département'}
+        adapt_is_applied = sva.patch_workflow('incomingmail_workflow', **n_plus_2_params)
+        if adapt_is_applied:
+            add_applied_adaptation('imio.dms.mail.wfadaptations.IMServiceValidation',
+                                   'incomingmail_workflow', True, **n_plus_2_params)
+        for uid in get_registry_organizations():
+            self.portal.acl_users.source_groups.addPrincipalToGroup('chef', "%s_n_plus_2" % uid)
+
+    def test_im_workflow2(self):
+        """ Check workflow """
+        self.assertSetEqual(set(self.imw.states),
+                            set(['created', 'proposed_to_manager', 'proposed_to_n_plus_2', 'proposed_to_n_plus_1',
+                                 'proposed_to_agent', 'in_treatment', 'closed']))
+        self.assertSetEqual(set(self.imw.transitions),
+                            set(['back_to_creation', 'back_to_manager', 'back_to_n_plus_2', 'back_to_n_plus_1',
+                                 'back_to_agent', 'back_to_treatment', 'propose_to_manager', 'propose_to_n_plus_2',
+                                 'propose_to_n_plus_1', 'propose_to_agent', 'treat', 'close']))
+        self.assertSetEqual(set(self.imw.states['created'].transitions),
+                            set(['propose_to_manager', 'propose_to_n_plus_2', 'propose_to_n_plus_1',
+                                 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_manager'].transitions),
+                            set(['back_to_creation', 'propose_to_n_plus_2', 'propose_to_n_plus_1', 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_n_plus_2'].transitions),
+                            set(['back_to_creation', 'back_to_manager', 'propose_to_n_plus_1', 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_n_plus_1'].transitions),
+                            set(['back_to_creation', 'back_to_manager', 'back_to_n_plus_2', 'propose_to_agent']))
+        self.assertSetEqual(set(self.imw.states['proposed_to_agent'].transitions),
+                            set(['back_to_creation', 'back_to_manager', 'back_to_n_plus_2', 'back_to_n_plus_1',
+                                 'treat', 'close']))
+        self.assertSetEqual(set(self.imw.states['in_treatment'].transitions),
+                            set(['back_to_agent', 'close']))
+        self.assertSetEqual(set(self.imw.states['closed'].transitions),
+                            set(['back_to_treatment', 'back_to_agent']))
+
+    def test_IMServiceValidation2(self):
+        """
+            Test IMServiceValidation adaptations
+        """
+        # is function added
+        self.assertIn('n_plus_2', [fct['fct_id'] for fct in get_registry_functions()])
+        # is local roles modified
+        fti = getUtility(IDexterityFTI, name='dmsincomingmail')
+        lr = getattr(fti, 'localroles')
+        self.assertIn('proposed_to_n_plus_2', lr['static_config'])
+        self.assertIn('proposed_to_n_plus_2', lr['treating_groups'])
+        self.assertIn('proposed_to_n_plus_2', lr['recipient_groups'])
+        # check collection
+        folder = self.portal['incoming-mail']['mail-searches']
+        self.assertIn('searchfor_proposed_to_n_plus_2', folder)
+        self.assertEqual(folder.getObjectPosition('searchfor_proposed_to_agent'), 14)
+        self.assertEqual(folder.getObjectPosition('searchfor_proposed_to_n_plus_1'), 13)
+        self.assertEqual(folder.getObjectPosition('searchfor_proposed_to_n_plus_2'), 12)
+        self.assertFalse(folder['to_treat_in_my_group'].showNumberOfItems)
+        # check annotations
+        config = get_dms_config(['review_levels', 'dmsincomingmail'])
+        self.assertIn('_n_plus_2', config)
+        config = get_dms_config(['review_states', 'dmsincomingmail'])
+        self.assertIn('proposed_to_n_plus_2', config)
+        config = get_dms_config(['transitions_auc', 'dmsincomingmail'])
+        self.assertIn('propose_to_n_plus_2', config)
+        config = get_dms_config(['transitions_levels', 'dmsincomingmail'])
+        self.assertEqual(config['proposed_to_manager'].values()[0][0], 'propose_to_n_plus_2')
+        self.assertEqual(config['proposed_to_n_plus_2'].values()[0][0], 'propose_to_n_plus_1')
+        self.assertEqual(config['proposed_to_n_plus_1'].values()[0][1], 'back_to_n_plus_2')
+        self.assertEqual(config['proposed_to_agent'].values()[0][1], 'back_to_n_plus_1')
+        # check vocabularies
+        factory = getUtility(IVocabularyFactory, u'collective.eeafaceted.collectionwidget.cachedcollectionvocabulary')
+        self.assertEqual(len(factory(folder, folder)), 17)
+        factory = getUtility(IVocabularyFactory, u'imio.dms.mail.IMReviewStatesVocabulary')
+        self.assertEqual(len(factory(folder)), 7)
+        # check configuration
+        lst = api.portal.get_registry_record('imio.actionspanel.browser.registry.IImioActionsPanelConfig.transitions')
+        self.assertIn('dmsincomingmail.back_to_n_plus_2|', lst)
+        lst = api.portal.get_registry_record('imio.dms.mail.browser.settings.IImioDmsMailConfig.imail_remark_states')
+        self.assertIn('proposed_to_n_plus_2', lst)
+
+    def test_IdmUtilsMethods_proposed_to_n_plus_col_cond2(self):
+        folder = self.portal['incoming-mail']['mail-searches']
+        col1 = folder['searchfor_proposed_to_n_plus_1']
+        n_plus_1_view = IdmUtilsMethods(col1, col1.REQUEST)
+        col2 = folder['searchfor_proposed_to_n_plus_2']
+        n_plus_2_view = IdmUtilsMethods(col2, col2.REQUEST)
         self.assertFalse(n_plus_2_view.proposed_to_n_plus_col_cond())
+        login(self.portal, 'encodeur')
+        self.assertTrue(n_plus_2_view.proposed_to_n_plus_col_cond())
+        login(self.portal, 'agent')
+        self.assertFalse(n_plus_2_view.proposed_to_n_plus_col_cond())
+        api.group.add_user(groupname='abc_group_encoder', username='agent')
+        self.assertTrue(n_plus_2_view.proposed_to_n_plus_col_cond())
+        api.group.remove_user(groupname='abc_group_encoder', username='agent')
+        login(self.portal, 'dirg')
+        self.assertTrue(n_plus_2_view.proposed_to_n_plus_col_cond())
+        login(self.portal, 'chef')
+        self.assertTrue(n_plus_2_view.proposed_to_n_plus_col_cond())
         # Set N+2 to user, have to get an organization UID first
         contacts = self.portal['contacts']
         own_orga = contacts['plonegroup-organization']
         departments = own_orga.listFolderContents(contentFilter={'portal_type': 'organization'})
         self.portal.acl_users.source_groups.addPrincipalToGroup('agent1', "%s_n_plus_2" % departments[5].UID())
         login(self.portal, 'agent1')
-        self.assertTrue(n_plus_1_view.proposed_to_n_plus_col_cond())
+        self.assertTrue(n_plus_1_view.proposed_to_n_plus_col_cond())  # can view lower level collection
         self.assertTrue(n_plus_2_view.proposed_to_n_plus_col_cond())
