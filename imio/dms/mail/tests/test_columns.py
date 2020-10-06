6ab3ca0aa72d6d2a8cc4fc1c7afc9cdde9b5bac6
Stephan Geulette <s.geulette@imio.be>
2020-10-06 17:44:49 +0200

    Updated test for imio.prettylink 1.17

--- imio/dms/mail/tests/test_columns.py
+++ imio/dms/mail/tests/test_columns.py
@@ -65,14 +65,16 @@ class TestColumns(unittest.TestCase):
                          u"<a class='pretty_link' title='E0001 - Courrier 1' "
                          u"href='http://nohost/plone/incoming-mail/courrier1' target='_blank'>"
                          u"<span class='pretty_link_icons'><img title='Incoming Mail' "
-                         u"src='http://nohost/plone/++resource++dmsincomingmail_icon.png' /></span><span class='"
+                         u"src='http://nohost/plone/++resource++dmsincomingmail_icon.png' style=\"width: 16px; "
+                         u"height: 16px;\" /></span><span class='"
                          u"pretty_link_content state-created'>E0001 - Courrier 1</span></a>")
         brain = self.portal.portal_catalog(UID=self.ta31.UID())[0]
         self.assertEqual(column.renderCell(brain),
                          u"<a class='pretty_link' title='E0001 - Courrier 1' "
                          u"href='http://nohost/plone/incoming-mail/courrier1' target='_blank'>"
                          u"<span class='pretty_link_icons'><img title='Incoming Mail' "
-                         u"src='http://nohost/plone/++resource++dmsincomingmail_icon.png' /></span><span class='"
+                         u"src='http://nohost/plone/++resource++dmsincomingmail_icon.png' style=\"width: 16px; "
+                         u"height: 16px;\" /></span><span class='"
                          u"pretty_link_content state-created'>E0001 - Courrier 1</span></a>")
 
     def test_TaskActionsColumn(self):
