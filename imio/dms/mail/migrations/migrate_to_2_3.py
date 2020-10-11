2fcd5c4947beaaa3201980e79eca08a37084de69
Stephan Geulette <s.geulette@imio.be>
2020-10-11 15:01:55 +0200

    Corrected message

--- imio/dms/mail/migrations/migrate_to_2_3.py
+++ imio/dms/mail/migrations/migrate_to_2_3.py
@@ -153,14 +153,11 @@ class Migrate_To_2_3(Migrator):  # noqa
             api.content.delete(self.portal['messages-config']['doc'])
         add_message('doc', 'Documentation', u'<p>Vous pouvez consulter la <a href="https://docs.imio.be/'
                     u'imio-doc/ia.docs/" target="_blank">documentation en ligne de la '
-                    u'dernière version</a>, ainsi que d\'autres documentations liées.</p>', msg_type='significant',
-                    can_hide=True, req_roles=['Authenticated'], activate=True)
+                    u'dernière version</a>, dont <a href="https://docs.imio.be/imio-doc/ia.docs/changelog" '
+                    u'target="_blank">les nouvelles fonctionnalités> ainsi que d\'autres documentations liées.</p>',
+                    msg_type='significant', can_hide=True, req_roles=['Authenticated'], activate=True)
         if 'new-version' in self.portal['messages-config']:
             api.content.delete(self.portal['messages-config']['new-version'])
-        add_message('new-version', 'Nouvelles fonctionnalités', u'<p>Vous pouvez consulter la <a href="https://'
-                    u'docs.imio.be/imio-doc/ia.docs/introduction/nouveautes.html" target="_blank">liste des nouvelles '
-                    u'fonctionnalités</a></p>', msg_type='significant', can_hide=True, req_roles=['Authenticated'],
-                    activate=True)
         # update plonegroup
         if not get_registry_groups_mgt():
             set_registry_groups_mgt(['dir_general', 'encodeurs', 'expedition'])
