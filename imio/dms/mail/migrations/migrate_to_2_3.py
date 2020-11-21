f7c58fdf412be8b1d4152301d3ec2359b53603d9
Stephan Geulette <s.geulette@imio.be>
2020-11-21 10:31:40 +0100

    Check if wfadaptations is well installed

--- imio/dms/mail/migrations/migrate_to_2_3.py
+++ imio/dms/mail/migrations/migrate_to_2_3.py
@@ -75,6 +75,8 @@ class Migrate_To_2_3(Migrator):  # noqa
         self.upgradeProfile('collective.contact.plonegroup:default')
         self.install(['collective.contact.importexport', 'collective.fontawesome'])
         self.runProfileSteps('plonetheme.imioapps', steps=['viewlets'])  # to hide messages-viewlet
+        if not self.portal_quickinstaller.isProductInstalled('collective.wfadaptations'):
+            self.install('collective.wfadaptations')
         self.runProfileSteps('imio.dms.mail', steps=['actions', 'plone.app.registry'], run_dependencies=False)
 
         # add new task collection
