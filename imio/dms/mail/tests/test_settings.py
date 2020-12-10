26eab4ed87d8a3aacb2349848a576272b6e2b555
Stephan Geulette <s.geulette@imio.be>
2020-12-10 14:18:20 +0100

    Added test_validate_settings

--- imio/dms/mail/tests/test_settings.py
+++ imio/dms/mail/tests/test_settings.py
@@ -5,14 +5,18 @@ from collective.contact.plonegroup.config import FUNCTIONS_REGISTRY
 from collective.contact.plonegroup.config import get_registry_functions
 from dexterity.localrolesfield.utils import get_localrole_fields
 from eea.facetednavigation.criteria.interfaces import ICriteria
+from imio.dms.mail import _tr
 from imio.dms.mail import CONTACTS_PART_SUFFIX
 from imio.dms.mail import CREATING_GROUP_SUFFIX
+from imio.dms.mail.browser.settings import IImioDmsMailConfig
 from imio.dms.mail.Extensions.demo import activate_group_encoder
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
+from plone import api
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.registry.interfaces import IRegistry
+from z3c.form import validator
 from zope.component import getUtility
 from zope.interface import Invalid
 from zope.schema.interfaces import IVocabularyFactory
@@ -29,6 +33,28 @@ class TestSettings(unittest.TestCase):
         """Custom shared utility setup for tests."""
         self.portal = self.layer['portal']
         self.registry = getUtility(IRegistry)
+
+    def test_validate_settings(self):
+        """ Check invariant """
+        invariants = validator.InvariantsValidator(None, None, None, IImioDmsMailConfig, None)
+        # test omail_send_modes part
+        data = {'omail_send_modes': [{'dtitle': u'Lettre', 'active': True, 'value': u'email'}]}
+        self.assertFalse(invariants.validate(data))
+        data = {'omail_send_modes': [{'dtitle': u'Lettre', 'active': True, 'value': u'bad'}]}
+        errors = invariants.validate(data)
+        self.assertTrue(isinstance(errors[0], Invalid))
+        error_msg = _tr(msgid=u"Outgoingmail tab: send_modes field must have values starting with 'post' or 'email'")
+        self.assertEqual(_tr(errors[0].message), error_msg)
+        # test imail_group_encoder
+        rec = 'imio.dms.mail.browser.settings.IImioDmsMailConfig.imail_group_encoder'
+        api.portal.set_registry_record(rec, True)
+        data = {'omail_send_modes': [], 'imail_group_encoder': False}  # needs omail_send_modes !!
+        errors = invariants.validate(data)
+        self.assertTrue(isinstance(errors[0], Invalid))
+        error_msg = u"Courrier entrant: décocher le paramètre 'Activation de plusieurs groupes d'indicatage' n'est " \
+                    u"pas prévu !"
+        self.assertEqual(_tr(errors[0].message, mapping={'tab': _tr('Incoming mail'),
+                                                         'field': _tr('Activate group encoder')}), error_msg)
 
     def test_imiodmsmail_settings_changed(self):
         """ Test some settings change """
@@ -51,10 +77,6 @@ class TestSettings(unittest.TestCase):
         self.assertEqual(len(self.registry[FUNCTIONS_REGISTRY]), 3)
         self.registry[key] = True
         self.assertEqual(len(self.registry[FUNCTIONS_REGISTRY]), 4)
-        with self.assertRaises(Invalid) as cm:
-            self.registry[key] = False
-        self.assertEqual(cm.exception.message, u"Décocher le paramètre 'Activation de plusieurs groupes d'indicatage' "
-                                               u"pour le courrier entrant n'est pas prévu !!")
 
     def test_configure_group_encoder(self):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
