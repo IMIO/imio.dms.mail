39a2e8f895ad11cd90937b11f003e0f77d0479b1
Stephan Geulette <s.geulette@imio.be>
2021-06-15 16:04:26 +0200

    Improved catalog test

--- imio/dms/mail/tests/ptest_mass.py
+++ imio/dms/mail/tests/ptest_mass.py
@@ -3,10 +3,12 @@ from __future__ import print_function
 
 from imio.dms.mail.testing import create_im_mails
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
+from imio.dms.mail.tests.ptest_performance import check_catalog_following_groups
 from plone.app.testing import login
 from plone.app.testing import logout
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
+from plone.app.testing import TEST_USER_NAME
 from profilehooks import timecall
 
 import unittest
@@ -21,8 +23,11 @@ class TestMass(unittest.TestCase):
         setRoles(self.portal, TEST_USER_ID, ['Manager'])
 
     def test_mass0(self):
-        nb = 100
-        print('Creating {} incoming mails'.format(nb))
-        login(self.portal, 'encodeur')
-        create_im_mails(self, nb, start=1, senders=[], transitions=['propose_to_agent'])
-        logout()
+        for j in range(0, 10):
+            nb = 1000
+            create_im_mails(self, nb*(j+1), start=nb*j+1, senders=[], transitions=['propose_to_agent'])
+            check_catalog_following_groups(self, init=True)
+            create_im_mails(self, 2000, start=1001, senders=[], transitions=['propose_to_agent'])
+            check_catalog_following_groups(self)
+            create_im_mails(self, 3000, start=2001, senders=[], transitions=['propose_to_agent'])
+            check_catalog_following_groups(self)
