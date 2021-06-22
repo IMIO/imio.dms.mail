e65d44922eabd61c9cf53652337d7b393ed15d91
Benoit Leybaert <benoit.leybaert@imio.be>
2021-06-22 17:13:25 +0200

    Add classification folder references to outgoing mail example

--- imio/dms/mail/browser/documentgenerator.py
+++ imio/dms/mail/browser/documentgenerator.py
@@ -152,6 +152,16 @@ class OMDGHelper(DXDocumentGenerationHelperView):
             ret[-1] = sep.join(parts[nb-1:])
         return ret
 
+    def get_classification_folders(self):
+        om = self.real_context
+        if not om.classification_folders:
+            return []
+        ret = []
+        for fld in om.classification_folders:
+            obj = uuidToObject(fld)
+            ret.append(obj.internal_reference_no)
+        return ret
+
 
 class DashboardDGBaseHelper():  # noqa
     """
