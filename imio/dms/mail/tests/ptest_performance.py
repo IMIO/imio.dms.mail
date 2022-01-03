0649f09999034a9508e6315603a78398f1c049ca
Stephan Geulette <s.geulette@imio.be>
2022-01-03 15:24:50 +0100

    Used imio.helpers function

--- imio/dms/mail/tests/ptest_performance.py
+++ imio/dms/mail/tests/ptest_performance.py
@@ -1,28 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
-
 from collective.contact.plonegroup.utils import organizations_with_suffixes
 from collective.contact.plonegroup.utils import voc_selected_org_suffix_users
 from imio.dms.mail.testing import add_user_in_groups
 from imio.dms.mail.testing import create_groups
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
+from imio.helpers.cache import extract_wrapped
 from imio.pyutils.utils import ftimed
 from plone.app.testing import login
 from plone.app.testing import setRoles
 from plone.app.testing import TEST_USER_ID
 from plone.app.testing import TEST_USER_NAME
 from plone import api
-from types import FunctionType
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 
 import unittest
-
-
-def extract_wrapped(decorated):
-    """Returns function that was decorated"""
-    closure = (c.cell_contents for c in decorated.__closure__)
-    return next((c for c in closure if isinstance(c, FunctionType)), None)
 
 
 # Commons
@@ -73,7 +66,7 @@ class TestPerformance(unittest.TestCase):
             nb, ftimed(lambda: voc_selected_org_suffix_users(org_uid, ['editeur']), nb)[0]))
         print(u'voc_selected_org_suffix_users ({}) with 5 n_plus: in {}'.format(
             nb, ftimed(lambda: voc_selected_org_suffix_users(org_uid, ['editeur', 'n_plus_1', 'n_plus_2',
-                                                                      'n_plus_3', 'n_plus_4', 'n_plus_5']), nb)[0]))
+                                                                       'n_plus_3', 'n_plus_4', 'n_plus_5']), nb)[0]))
 
     def test_organizations_with_suffixes(self):
         org_uid = self.portal.contacts['plonegroup-organization']['direction-generale']['secretariat'].UID()
