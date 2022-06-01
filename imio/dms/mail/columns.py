b8efc59a50da884be79d6722dcdae3cf5b2862d1
Stephan Geulette <s.geulette@imio.be>
2022-06-01 16:24:05 +0200

    Removed useless param when using escape

--- imio/dms/mail/columns.py
+++ imio/dms/mail/columns.py
@@ -157,8 +157,7 @@ class ContactsColumn(PrettyLinkColumn):
                 ret.append(u"<a href='%s' target='_blank' class='pretty_link link-tooltip'>"
                            u"<span class='pretty_link_icons'>%s</span>"
                            u"<span class='pretty_link_content'>%s</span></a>"
-                           % (c_brain.getURL(), self._icons(c_brain), safe_unicode(html.escape(c_brain.get_full_title,
-                                                                                               quote=True)))
+                           % (c_brain.getURL(), self._icons(c_brain), safe_unicode(html.escape(c_brain.get_full_title)))
                            )
         l_ret = len(ret)
         if l_ret == 1:
@@ -420,7 +419,7 @@ class PathColumn(LinkColumn, BaseColumn):
         rel_path = os.path.relpath(dir_path, self.root_path)
         if rel_path not in self.paths:
             self.rel_path_title(rel_path)
-        return html.escape(self.paths[rel_path], quote=True)
+        return html.escape(self.paths[rel_path])
 
 # ck-templates-listing columns
 
