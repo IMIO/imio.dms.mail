88c0e171fc90052b73c3e0809b44804e82f9c345
Stephan Geulette <s.geulette@imio.be>
2022-07-13 14:16:18 +0200

    Replaced task assigned_user vocabulary by SimplySortedUsers vocabulary with userid as value

--- imio/dms/mail/tests/test_browser_task.py
+++ imio/dms/mail/tests/test_browser_task.py
@@ -3,6 +3,9 @@ from collective.contact.plonegroup.config import get_registry_organizations
 from imio.dms.mail.browser.task import filter_task_assigned_users
 from imio.dms.mail.testing import DMSMAIL_INTEGRATION_TESTING
 from imio.helpers.test_helpers import ImioTestHelpers
+from imio.helpers.vocabularies import SimplySortedUsers
+from zope.component import getUtility
+from zope.schema.interfaces import IVocabularyFactory
 
 import unittest
 
@@ -22,3 +25,14 @@ class TestBrowserTask(unittest.TestCase, ImioTestHelpers):
         self.assertListEqual([t.title for t in voc._terms], [])  # direction generale => no user
         voc = filter_task_assigned_users(selected_orgs[1])
         self.assertListEqual([t.title for t in voc._terms], [u'Fred Agent'])
+
+    def test_AssignedUsersVocabulary(self):
+        voc_inst = getUtility(IVocabularyFactory, 'collective.task.AssignedUsers')
+        self.assertIsInstance(voc_inst, SimplySortedUsers)
+        # value is userid and not username
+        self.assertListEqual(sorted([t.value for t in voc_inst(None)]),
+                             ['agent', 'agent1', 'chef', 'dirg', 'encodeur', 'lecteur', 'scanner', 'siteadmin',
+                              'test_user_1_'])
+        self.assertListEqual([t.title for t in voc_inst(None)],
+                             [u'Fred Agent', u'Jean Encodeur', u'Jef Lecteur', u'Maxime DG', u'Michel Chef',
+                              u'siteadmin', u'Scanner', u'Stef Agent', u'test_user_1_'])
