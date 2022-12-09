770851171097cf63909c9fa7b7baedadd44769d0
Stephan Geulette <s.geulette@imio.be>
2022-12-09 17:38:14 +0100

    Modified solr maintenance view to insert global BATCH mode

--- imio/dms/mail/browser/overrides.py
+++ imio/dms/mail/browser/overrides.py
@@ -9,6 +9,7 @@
 
 from Acquisition import aq_inner  # noqa
 from Acquisition import aq_parent
+from BTrees.IIBTree import IITreeSet
 from collective.ckeditortemplates.browser.cktemplatelisting import CKTemplateListingView
 from collective.ckeditortemplates.cktemplate import ICKTemplate
 from collective.classification.folder.content.classification_folder import IClassificationFolder
@@ -37,6 +38,7 @@ from imio.dms.mail.interfaces import IIMDashboard
 from imio.dms.mail.interfaces import IOMDashboard
 from imio.history.browser.views import IHDocumentBylineViewlet
 from imio.pyutils.system import read_dictcsv
+from logging import getLogger
 from plone import api
 from plone.app.controlpanel.usergroups import GroupsOverviewControlPanel
 from plone.app.controlpanel.usergroups import UsersGroupsControlPanelView
@@ -48,6 +50,7 @@ from plone.locking.browser.info import LockInfoViewlet as PLLockInfoViewlet
 from plone.locking.browser.locking import LockingOperations as PLLockingOperations
 from Products.ATContentTypes.interfaces.document import IATDocument
 from Products.ATContentTypes.interfaces.folder import IATBTreeFolder
+from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import utils
 from Products.CMFPlone.browser.navigation import get_view_url
 from Products.CMFPlone.browser.navigation import PhysicalNavigationBreadcrumbs as PlonePhysicalNavigationBreadcrumbs
@@ -55,12 +58,16 @@ from Products.CMFPlone.browser.ploneview import Plone as PloneView
 from Products.CMFPlone.interfaces import IHideFromBreadcrumbs
 from Products.CMFPlone.interfaces.siteroot import IPloneSiteRoot
 from Products.CPUtils.Extensions.utils import check_zope_admin
+from Products.Five import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from time import clock
 from zope.annotation import IAnnotations
 from zope.component import getMultiAdapter
+from zope.component import queryUtility
 from zope.interface.exceptions import Invalid
 
 import os
+import sys
 
 
 class IMRenderCategoryView(RenderCategoryView):
@@ -324,3 +331,169 @@ class DocsImportFormSecondStep(ImportFormSecondStep):
 class DocsImportSecondStepView(ImportSecondStepView):
 
     form = DocsImportFormSecondStep
+
+
+# collective.solr maintenance view
+
+try:
+    from collective.solr.browser.maintenance import checkpointIterator
+    from collective.solr.browser.maintenance import MAX_ROWS
+    from collective.solr.browser.maintenance import notimeout
+    from collective.solr.browser.maintenance import SolrMaintenanceView
+    from collective.solr.browser.maintenance import timer
+    from collective.solr.indexer import SolrIndexProcessor
+    from collective.solr.interfaces import ICheckIndexable
+    from collective.solr.interfaces import ISolrConnectionManager
+    from collective.solr.parser import parse_date_as_datetime
+    from collective.solr.parser import SolrResponse
+    from collective.solr.parser import unmarshallers
+    BaseMaintenanceView = SolrMaintenanceView
+except ImportError:
+    BaseMaintenanceView = BrowserView
+
+logger = getLogger('collective.solr.maintenance docs')
+
+
+class DocsSolrMaintenanceView(BaseMaintenanceView):
+    """Overrides sync method to take into account a BATCH """
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.batch_value = int(os.getenv('BATCH', '0'))
+
+    def sync(self, batch=1000, preImportDeleteQuery='*:*'):
+        """Sync the Solr index with the portal catalog. Records contained
+        in the catalog but not in Solr will be indexed and records not
+        contained in the catalog will be removed.
+        Overrided following c99d607cc9af47ca7614ba09016b17483e441ce5 version
+        """
+        manager = queryUtility(ISolrConnectionManager)
+        proc = SolrIndexProcessor(manager)
+        conn = manager.getConnection()
+        key = queryUtility(ISolrConnectionManager).getSchema().uniqueKey
+        zodb_conn = self.context._p_jar
+        catalog = getToolByName(self.context, 'portal_catalog')
+        getIndex = catalog._catalog.getIndex
+        modified_index = getIndex('modified')
+        uid_index = getIndex(key)
+        log = self.mklog()
+        real = timer()          # real time
+        lap = timer()           # real lap time (for intermediate commits)
+        cpu = timer(clock)      # cpu time
+        # get Solr status
+        response = conn.search(
+            q=preImportDeleteQuery,
+            rows=MAX_ROWS,
+            fl='%s modified' % key)
+        # avoid creating DateTime instances
+        simple_unmarshallers = unmarshallers.copy()
+        simple_unmarshallers['date'] = parse_date_as_datetime
+        flares = SolrResponse(response, simple_unmarshallers)
+        response.close()
+        solr_results = {}
+        solr_uids = set()
+
+        def _utc_convert(value):
+            t_tup = value.utctimetuple()
+            return ((((t_tup[0] * 12 + t_tup[1]) * 31 + t_tup[2])
+                    * 24 + t_tup[3]) * 60 + t_tup[4])
+        for flare in flares:
+            uid = flare[key]
+            solr_uids.add(uid)
+            solr_results[uid] = _utc_convert(flare['modified'])
+        # get catalog status
+        cat_results = {}
+        cat_uids = set()
+        for uid, rid in uid_index._index.items():
+            cat_uids.add(uid)
+            cat_results[uid] = rid
+        # differences
+        index = cat_uids.difference(solr_uids)
+        solr_uids.difference_update(cat_uids)
+        unindex = solr_uids
+        processed = 0
+        flush = notimeout(lambda: conn.flush())
+
+        def checkPoint():
+            msg = 'intermediate commit (%d items processed, ' \
+                  'last batch in %s)...\n' % (processed, lap.next())
+            log(msg)
+            logger.info(msg)
+            flush()
+            zodb_conn.cacheGC()
+            if self.batch_value and processed >= self.batch_value:
+                logger.info('EXITED following BATCH env value {}'.format(self.batch_value))
+                sys.exit(0)
+
+        cpi = checkpointIterator(checkPoint, batch)
+        # Look up objects
+        uid_rid_get = cat_results.get
+        rid_path_get = catalog._catalog.paths.get
+        catalog_traverse = catalog.unrestrictedTraverse
+
+        def lookup(uid, rid=None,
+                   uid_rid_get=uid_rid_get, rid_path_get=rid_path_get,
+                   catalog_traverse=catalog_traverse):
+            if rid is None:
+                rid = uid_rid_get(uid)
+            if not rid:
+                return None
+            if not isinstance(rid, int):
+                rid = tuple(rid)[0]
+            path = rid_path_get(rid)
+            if not path:
+                return None
+            try:
+                obj = catalog_traverse(path)
+            except AttributeError:
+                return None
+            return obj
+        log('processing %d "unindex" operations next...\n' % len(unindex))
+        op = notimeout(lambda uid: conn.delete(id=uid))
+        for uid in unindex:
+            obj = lookup(uid)
+            if obj is None:
+                op(uid)
+                processed += 1
+                cpi.next()
+            else:
+                log('not unindexing existing object %r.\n' % uid)
+        log('processing %d "index" operations next...\n' % len(index))
+        op = notimeout(lambda obj: proc.index(obj))
+        for uid in index:
+            obj = lookup(uid)
+            if ICheckIndexable(obj)():
+                op(obj)
+                processed += 1
+                cpi.next()
+            else:
+                log('not indexing unindexable object %r.\n' % uid)
+            if obj is not None:
+                obj._p_deactivate()
+        log('processing "reindex" operations next...\n')
+        op = notimeout(lambda obj: proc.reindex(obj))
+        cat_mod_get = modified_index._unindex.get
+        solr_mod_get = solr_results.get
+        done = unindex.union(index)
+        for uid, rid in cat_results.items():
+            if uid in done:
+                continue
+            if isinstance(rid, IITreeSet):
+                rid = rid.keys()[0]
+            if cat_mod_get(rid) != solr_mod_get(uid):
+                obj = lookup(uid, rid=rid)
+                if ICheckIndexable(obj)():
+                    op(obj)
+                    processed += 1
+                    cpi.next()
+                else:
+                    log('not reindexing unindexable object %r.\n' % uid)
+                if obj is not None:
+                    obj._p_deactivate()
+        conn.commit()
+        log('solr index synced.\n')
+        msg = 'processed %d object(s) in %s (%s cpu time).'
+        msg = msg % (processed, real.next(), cpu.next())
+        log(msg)
+        logger.info(msg)
