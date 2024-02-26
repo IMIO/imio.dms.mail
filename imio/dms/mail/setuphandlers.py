6d3fdac86c344bd50b739609d0423d76bb7d85a4
Stephan Geulette <s.geulette@imio.be>
2024-02-26 10:39:25 +0100

    Improved footer

--- imio/dms/mail/setuphandlers.py
+++ imio/dms/mail/setuphandlers.py
@@ -1211,7 +1211,7 @@ def adaptDefaultPortal(context):
     api.portal.set_registry_record('collective.contact.core.interfaces.IContactCoreParameters.'
                                    'contact_source_metadata_content',
                                    u'{gft} ⏺ {number}, {street}, {zip_code}, {city} ⏺ {email}')
-    # chars ⏺, ↈ  , ▐ , ⬤ (see ubuntu character table. Use ctrl+shift+u+code)
+    # chars ⏺, ↈ  , ▐ , ⬤, ● (see ubuntu character table. Use ctrl+shift+u+code)
     api.portal.set_registry_record('collective.contact.core.interfaces.IContactCoreParameters.'
                                    'display_below_content_title_on_views', True)
     # imio.dms.mail configuration annotation
